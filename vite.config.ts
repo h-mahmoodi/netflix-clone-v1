0a757dc2ff5e197131afe2f6e3663f116ec9f602
h-mahmoodi <hesamahmoodi@gmail.com>
2024-11-20 10:30:05 +0100

    adding deployment CI

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@src": "/src",
-      "@components": "/src/components",
-      "@hooks": "/src/hooks",
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/netflix-clone-v1/" : "/",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@src": "/src",
+        "@components": "/src/components",
+        "@hooks": "/src/hooks",
+      },
     },
-  },
+  };
 });
