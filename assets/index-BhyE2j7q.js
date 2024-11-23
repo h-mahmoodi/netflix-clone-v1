eba746dcbcd71ef6e2574d86f6c03498225f8585
h-mahmoodi <h-mahmoodi@users.noreply.github.com>
2024-11-23 23:49:46 +0000

    deploy: 45a429e61a51a2822e69fc4d4719ca28be6602f3

---
+++ assets/index-BhyE2j7q.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-l2fMN52j.js";const t="_skeleton_a3d35_1",o="_shimmer_a3d35_1",n={skeleton:t,shimmer:o},r=({className:s})=>e.jsx("div",{className:`${s} ${n.skeleton} `});export{r as S};
