6ef4eeb91fd2465ad11aa03fd0139c0f1686f711
h-mahmoodi <h-mahmoodi@users.noreply.github.com>
2024-11-26 14:44:21 +0000

    deploy: cf6729e983929972ac672d13ac526243f962b48d

---
+++ assets/index-C_LjZVMI.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-qQ8MpHoT.js";const t="_skeleton_a3d35_1",o="_shimmer_a3d35_1",n={skeleton:t,shimmer:o},r=({className:s})=>e.jsx("div",{className:`${s} ${n.skeleton} `});export{r as S};
