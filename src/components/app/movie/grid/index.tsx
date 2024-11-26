b9373a1233ccf1e6444ec96853f070a8e4cfc1af
h-mahmoodi <hesamahmoodi@gmail.com>
2024-11-26 15:58:16 +0100

    update loading state for movie grid

--- src/components/app/movie/grid/index.tsx
+++ src/components/app/movie/grid/index.tsx
@@ -10,18 +10,34 @@ type MovieGridProps = {
   grid?: number;
 };
 
-const MovieGrid = forwardRef<HTMLDivElement, MovieGridProps>(({ movies, grid = 5 }, ref) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.gridLayout}>
-        {movies?.map((movie, index) => (
-          <div className={styles[`width-1-${grid}`]} key={`${movie.id}-${index}`}>
-            <MovieCard movie={movie} />
+const MovieGrid = forwardRef<HTMLDivElement, MovieGridProps>(
+  ({ movies, grid = 5, isLoading }, ref) => {
+    if (isLoading) {
+      return (
+        <div className={styles.container}>
+          <div className="flex justify-center items-center w-full">
+            <span className="flex items-center mt-20 px-3 text-xl animate-spin ">
+              <i className="flex fi fi-rr-spinner text-5xl text-zinc-700"></i>
+            </span>
           </div>
-        ))}
+        </div>
+      );
+    }
+    return (
+      <div className={styles.container}>
+        <div className={styles.gridLayout}>
+          {movies?.map((movie, index) => (
+            <div
+              className={styles[`width-1-${grid}`]}
+              key={`${movie.id}-${index}`}
+            >
+              <MovieCard movie={movie} />
+            </div>
+          ))}
+        </div>
+        <div className={styles.moreButton} ref={ref}></div>
       </div>
-      <div className={styles.moreButton} ref={ref}></div>
-    </div>
-  );
-});
+    );
+  }
+);
 export default MovieGrid;
